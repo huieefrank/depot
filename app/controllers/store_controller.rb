72eee0bbc05a27cd0a04743823e4c43622d2c09b
Frank <frank_fan@huieee.com>
2010-05-26 09:43:52 +0800

    complete dash of ajax

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,14 +1,20 @@
 class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
+    @cart = find_cart
   end
 
 
   
   def add_to_cart
     product = Product.find(params[:id]) 
-    @cart = find_cart                   
-    @cart.add_product(product)  
+    @cart = find_cart                
+    
+    @current_item = @cart.add_product(product) 
+     respond_to do |format|
+      format.js if request.xhr?
+	  format.html {redirect_to_index}
+    end 
     rescue ActiveRecord::RecordNotFound
     logger.error("Attempt to access invalid product #{params[:id]}")
     redirect_to_index("Invalid product" )        
@@ -17,7 +23,7 @@ class StoreController < ApplicationController
   
   def empty_cart
     session[:cart] = nil
-    redirect_to_index("Your cart is currently empty" )
+    redirect_to_index
   end
 
 
@@ -27,8 +33,8 @@ private
     session[:cart] ||= Cart.new
   end
   
-  def redirect_to_index(msg)
-	flash[:notice] = msg
+  def redirect_to_index(msg=nil)
+	flash[:notice] = msg if msg
 	redirect_to :action => 'index'
   end
 
